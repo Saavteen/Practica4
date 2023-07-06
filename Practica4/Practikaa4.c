56a307cdc262095326be9f0967e57c1e200f1a6d
Saavteen <den766sav@gmail.com>
2023-07-06 16:54:28 +0300

    Добавьте файлы проекта.

---
+++ Practica4/Practikaa4.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int countEqualDivisors(int n) {
+    int count = 0;
+    for (int m = 1; m <= n; m++) {
+        if (n / m == n % m) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    printf("Введіть натуральне число n: ");
+    scanf("%d", &n);
+
+    int result = countEqualDivisors(n);
+    printf("Кількість рівних дільників числа %d: %d\n", n, result);
+
+    return 0;
+}
